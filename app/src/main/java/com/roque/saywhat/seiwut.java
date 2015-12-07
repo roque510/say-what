c004475eabb9084b06edcafc917ef1fa995e36f9
Armando <roque510@hotmail.com>
2015-12-06 19:49:33 -0600

    Initial commit

---
+++ app/src/main/java/com/roque/saywhat/seiwut.java
@@ -0,0 +1,13 @@
+package com.roque.saywhat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class seiwut extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seiwut);
+    }
+}
